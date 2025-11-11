4f4a1bd2f3f537a141b372fcdd0eaf67d421c03a
iamfab <fabian2015hd@gmail.com>
2025-11-11 16:43:26 -0400

    refactor: entity sections add column nav_item_id and endpoint get sections by nav-item-uuid

--- src/main/java/com/infsis/socialpagebackend/sections/models/Section.java
+++ src/main/java/com/infsis/socialpagebackend/sections/models/Section.java
@@ -3,6 +3,7 @@ package com.infsis.socialpagebackend.sections.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.infsis.socialpagebackend.authentication.models.Users;
 import com.infsis.socialpagebackend.institutions.models.Institution;
+import com.infsis.socialpagebackend.navigation.models.NavItem;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -43,6 +44,10 @@ public class Section {
     @JoinColumn(name = "user_id", referencedColumnName = "uuid", nullable = false)
     private Users users;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nav_item_id", referencedColumnName = "uuid")
+    private NavItem navItem;
+
     @Column(nullable = false, length = 1000)
     private String name;
 
@@ -74,11 +79,11 @@ public class Section {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Section section = (Section) o;
-        return deleted == section.deleted && Objects.equals(id, section.id) && Objects.equals(uuid, section.uuid) && Objects.equals(institution, section.institution) && Objects.equals(users, section.users) && Objects.equals(name, section.name) && Objects.equals(date, section.date) && Objects.equals(articles, section.articles) && Objects.equals(createdDate, section.createdDate) && Objects.equals(lastModifiedDate, section.lastModifiedDate);
+        return deleted == section.deleted && Objects.equals(id, section.id) && Objects.equals(uuid, section.uuid) && Objects.equals(institution, section.institution) && Objects.equals(users, section.users) && Objects.equals(navItem, section.navItem) && Objects.equals(name, section.name) && Objects.equals(date, section.date) && Objects.equals(articles, section.articles) && Objects.equals(createdDate, section.createdDate) && Objects.equals(lastModifiedDate, section.lastModifiedDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, uuid, institution, users, name, date, articles, createdDate, lastModifiedDate, deleted);
+        return Objects.hash(id, uuid, institution, users, navItem, name, date, articles, createdDate, lastModifiedDate, deleted);
     }
 }
